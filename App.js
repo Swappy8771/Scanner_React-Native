a81886411d9002dd6eab4e61b0a1678d4d9f5190
swapnil <swapnilpatil8771@gmail.com>
2023-11-03 12:36:45 +0530

    Add first commit

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setData(data);
+  };
+
+  const handleScanAgain = () => {
+    setScanned(false);
+    setData('');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {hasPermission === null ? (
+        <Text>Requesting camera permission</Text>
+      ) : hasPermission === false ? (
+        <Text>No access to camera</Text>
+      ) : (
+        <>
+        {scanned ? (
+            <View style={styles.buttonContainer}>
+             
+            </View>
+          ) : null}
+          {data !== '' && <Text>Data: {data}</Text>}
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            style={{ height: 350, width: 350 }}
+          />
+          {scanned ? (
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={styles.scanAgainButton}
+                onPress={handleScanAgain}
+              >
+                <Text style={styles.buttonText}>Tap to Scan</Text>
+              </TouchableOpacity>
+            </View>
+          ) : null}
+          {''}
+        </>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  buttonContainer: {
+    marginTop: 20,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  scanAgainButton: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
   },
 });
